645a9c882148e4d3effd4c99bed823f349870ba7
userjeongwoo <148690776+userjeongwoo@users.noreply.github.com>
2023-10-24 17:34:39 +0900

    Add files via upload
    백준 A+B

---
+++ Test_000.java
@@ -0,0 +1,14 @@
+package Day231024;
+
+import java.util.Scanner;
+
+public class Test_000 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int A,B;
+		A=sc.nextInt();
+		B=sc.nextInt();
+		System.out.println(A+B);
+	}
+
+}
